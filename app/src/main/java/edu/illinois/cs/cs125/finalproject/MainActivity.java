815245b0b9326fc88c573e396ef0f212b7d9627c
apple <czep1111@hotmail.com>
2018-04-23 17:59:20 -0500

    !!!!

--- app/src/main/java/edu/illinois/cs/cs125/finalproject/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/finalproject/MainActivity.java
@@ -1,13 +1,105 @@
 package edu.illinois.cs.cs125.finalproject;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    /**
+     * Default logging tag for messages from the main activity.
+     */
+    private static final String TAG = "Final Project";
+
+    private String EVENT_DATE_TIME = "2019-12-31 10:30:00";
+    private String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    private LinearLayout linear_layout_1, linear_layout_2;
+    private TextView tv_days, tv_hour, tv_minute, tv_second;
+    private Handler handler = new Handler();
+    private Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initUI();
+        countDownStart();
+
+        final ImageButton openSideBar = findViewById(R.id.SideBar);
+        openSideBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Trick or Treat!");
+            }
+        });
+
+
+        final ImageButton openSettings = findViewById(R.id.Settings);
+        openSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (final View v){
+                Log.d(TAG, "Please set your date!");
+            }
+        });
+
     }
+
+    private void initUI() {
+        linear_layout_1 = findViewById(R.id.linear_layout_1);
+        linear_layout_2 = findViewById(R.id.linear_layout_2);
+        tv_days = findViewById(R.id.tv_days);
+        tv_hour = findViewById(R.id.tv_hour);
+        tv_minute = findViewById(R.id.tv_minute);
+        tv_second = findViewById(R.id.tv_second);
+    }
+
+    private void countDownStart() {
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    handler.postDelayed(this, 1000);
+                    SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+                    Date event_date = dateFormat.parse(EVENT_DATE_TIME);
+                    Date current_date = new Date();
+                    if (!current_date.after(event_date)) {
+                        long diff = event_date.getTime() - current_date.getTime();
+                        long Days = diff / (24 * 60 * 60 * 1000);
+                        long Hours = diff / (60 * 60 * 1000) % 24;
+                        long Minutes = diff / (60 * 1000) % 60;
+                        long Seconds = diff / 1000 % 60 ;
+                        //
+                        tv_days.setText(String.format("%02d", Days));
+                        tv_hour.setText(String.format("%02d", Hours));
+                        tv_minute.setText(String.format("%02d", Minutes));
+                        tv_second.setText(String.format("%02d", Seconds));
+                    } else {
+                        linear_layout_1.setVisibility(View.VISIBLE);
+                        linear_layout_2.setVisibility(View.GONE);
+                        handler.removeCallbacks(runnable);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        handler.postDelayed(runnable, 0);
+    }
+
+    protected void onStop() {
+        super.onStop();
+        handler.removeCallbacks(runnable);
+    }
+
 }
