5db4a78b30413810afe09ef9c47edb275b6452b1
apple <czep1111@hotmail.com>
2018-05-01 15:35:58 -0500

    11111

--- app/src/main/java/edu/illinois/cs/cs125/finalproject/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/finalproject/MainActivity.java
@@ -1,11 +1,10 @@
 package edu.illinois.cs.cs125.finalproject;
 
-import android.nfc.Tag;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.os.Handler;
 import android.widget.LinearLayout;
@@ -14,23 +13,72 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import android.content.Intent;
 
+import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+import android.view.LayoutInflater;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.content.Context;
+
+
 public class MainActivity extends AppCompatActivity {
     /**
      * Default logging tag for messages from the main activity.
      */
-    private static final String TAG = "Final Project";
 
-    private String EVENT_DATE_TIME = "2019-12-31 10:30:00";
+    public static String year = "";
+    public static String month = "";
+    public static String date = "";
+    private String EVENT_DATE_TIME = "";
     private String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
     private LinearLayout linear_layout_1, linear_layout_2;
     private TextView tv_days, tv_hour, tv_minute, tv_second;
     private Handler handler = new Handler();
     private Runnable runnable;
+    private static final String TAG = "Final Project";
+
+
+    EditText textIn;
+    Button buttonAdd;
+    LinearLayout container;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FileInputStream fis = null;
+        byte[] buffer = new byte[3];
+        try {
+            fis=openFileInput("login");
+            buffer=new byte[fis.available()];
+            fis.read(buffer);
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(fis!=null){
+                try {
+                    fis.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        TextView y = findViewById(R.id.year);
+        TextView m = findViewById(R.id.month);
+        TextView d = findViewById(R.id.date);
+
+        String data=new String(buffer);
+        year = data.split(" ")[0];
+        month = data.split(" ")[1];
+        date = data.split(" ")[2];
 
         initUI();
         countDownStart();
@@ -40,8 +88,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(final View v) {
                 Log.d(TAG, "Open Side Bar");
-                Snackbar message = Snackbar.make(findViewById(R.id.SideBar), "Successfully clicked this button!", Snackbar.LENGTH_SHORT);
-                message.show();
             }
         });
 
@@ -51,18 +97,47 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick (final View v){
                 Log.d(TAG, "Open Settings");
-                Snackbar message = Snackbar.make(findViewById(R.id.Settings), "Successfully clicked this button!", Snackbar.LENGTH_SHORT);
-                message.show();
                 setnewgoal(findViewById(R.id.Settings));
             }
         });
 
+        textIn = (EditText)findViewById(R.id.textin);
+        buttonAdd = (Button)findViewById(R.id.add);
+        container = (LinearLayout)findViewById(R.id.container);
+
+        buttonAdd.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                LayoutInflater layoutInflater =
+                        (LayoutInflater) getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View addView = layoutInflater.inflate(R.layout.row, null);
+                TextView textOut = (TextView)addView.findViewById(R.id.textout);
+                textOut.setText(textIn.getText().toString());
+                Button buttonRemove = (Button)addView.findViewById(R.id.remove);
+                buttonRemove.setOnClickListener(new OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        ((LinearLayout)addView.getParent()).removeView(addView);
+                    }
+                });
+
+                container.addView(addView);
+            }
+        });
     }
+
+
 
     public void setnewgoal(View view) {
         Intent intent = new Intent(this, SetUpYourGoal.class);
         startActivity(intent);
     }
+
+    public void inputdate() {
+
+    }
+
 
     private void initUI() {
         linear_layout_1 = findViewById(R.id.linear_layout_1);
@@ -78,17 +153,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
+                    EVENT_DATE_TIME = year + "-" + month + "-" + date + " 00:00:00";
                     handler.postDelayed(this, 1000);
                     SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
-                    Date event_date = dateFormat.parse(EVENT_DATE_TIME);
+                    Date event_date = dateFormat.parse(EVENT_DATE_TIME); //用户输入
                     Date current_date = new Date();
                     if (!current_date.after(event_date)) {
                         long diff = event_date.getTime() - current_date.getTime();
                         long Days = diff / (24 * 60 * 60 * 1000);
                         long Hours = diff / (60 * 60 * 1000) % 24;
                         long Minutes = diff / (60 * 1000) % 60;
-                        long Seconds = diff / 1000 % 60 ;
-                        //
+                        long Seconds = diff / 1000 % 60;
                         tv_days.setText(String.format("%02d", Days));
                         tv_hour.setText(String.format("%02d", Hours));
                         tv_minute.setText(String.format("%02d", Minutes));
--- app/src/main/java/edu/illinois/cs/cs125/finalproject/SetUpYourGoal.java
+++ app/src/main/java/edu/illinois/cs/cs125/finalproject/SetUpYourGoal.java
@@ -2,6 +2,14 @@ package edu.illinois.cs.cs125.finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import android.content.Intent;
+
 
 public class SetUpYourGoal extends AppCompatActivity {
 
@@ -9,5 +17,38 @@ public class SetUpYourGoal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_up_your_goal);
+        final EditText years =findViewById(R.id.year);
+        final EditText months =findViewById(R.id.month);
+        final EditText dates =findViewById(R.id.date);
+
+        Button login=findViewById(R.id.button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String year = years.getText().toString();
+                String month = months.getText().toString();
+                String date = dates.getText().toString();
+                FileOutputStream fos=null;
+                try {
+                    fos = openFileOutput("login",MODE_PRIVATE);
+                    fos.write((year+" "+month+" "+date).getBytes());
+                    fos.flush();
+                }catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }finally {
+                    if(fos!=null){
+                        try {
+                            fos.close();
+                        }catch (IOException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                Intent intent = new Intent(SetUpYourGoal.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
